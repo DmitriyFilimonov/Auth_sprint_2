9fe59331adbc2f42f87f2ccc281c7963654d1558
Dmitriy Filimonov <banodannon@gmail.com>
2026-02-21 03:02:20 +0300

    check tests after changes, WIP

--- auth-service/src/db/postgres.py
+++ auth-service/src/db/postgres.py
@@ -7,12 +7,12 @@ from src.core.config import settings
 Base = declarative_base()
 # Создаём движок
 # Настройки подключения к БД передаём из переменных окружения, которые заранее загружены в файл настроек
-dsn = f'{settings.async_driver}://{settings.postgres_user}:{settings.postgres_password}@{settings.postgres_host}:{settings.postgres_port}/{settings.postgres_db}'
+dsn = f"{settings.async_driver}://{settings.postgres_user}:{settings.postgres_password}@{settings.postgres_host}:{settings.postgres_port}/{settings.postgres_db}"
 
-migrations_dsn = f'{settings.sync_driver}://{settings.postgres_user}:{settings.postgres_password}@{settings.postgres_migrations_host}:{settings.postgres_port}/{settings.postgres_db}'
+migrations_dsn = f"{settings.sync_driver}://{settings.postgres_user}:{settings.postgres_password}@{settings.postgres_migrations_host}:{settings.postgres_port}/{settings.postgres_db}"
 
 engine = create_async_engine(dsn, echo=True, future=True)
-async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False) # type: ignore
+async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)  # type: ignore
 
 
 # Dependency
@@ -25,9 +25,3 @@ async def create_database() -> None:
     """Создание таблиц."""
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
-
-async def purge_database() -> None:
-    """Удаление таблиц."""
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
